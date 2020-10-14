d89b62623da71770eab15ce7b5d02e5b549dba77
NoHomey <ivo.stratev.tues@gmail.com>
2020-10-14 17:54:13 +0300

    adding Select

---
+++ src/component/formControl/Select.tsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import MuiSelect from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import comparator from './../../utility/compareProps';
+
+interface FormControlProps {
+    className?: string,
+    secondary?: boolean,
+    disabled?: boolean,
+    error?: string,
+    required?: boolean,
+    id: string
+}
+
+export interface SelectProps<Value extends string | number> extends FormControlProps {
+    helperText?: string,
+    label: string,
+    value: Value | '',
+    onValueChange: (value: Value) => void,
+    notSelectedText?: string,
+    options: Array<Value>
+    optionText: {
+        [V in Value]: string
+    }
+};
+
+function changeableProps<Value extends string | number>()
+    : Array<keyof SelectProps<Value>> {
+    return [
+        'disabled',
+        'helperText',
+        'error',
+        'value',
+        'label',
+        'notSelectedText',
+        'optionText'
+    ];
+}
+
+type ChangeEvent = React.ChangeEvent<{name?: string; value: unknown}>;
+
+function handleOnChangeEvent<Value extends string | number>(currentValue: Value | '', onValueChange: (_: Value) => void) {
+    return (event: ChangeEvent) => {
+        const eventValue = event.target.value as Value | '';
+        if(eventValue !== '' && currentValue !== eventValue) {
+            onValueChange(eventValue);
+        }
+    };
+}
+
+function Select<Value extends string | number>({
+    helperText,
+    label,
+    value,
+    onValueChange,
+    notSelectedText,
+    options,
+    optionText,
+    className,
+    secondary,
+    disabled,
+    error,
+    required,
+    id
+}: SelectProps<Value>): JSX.Element {
+    const isError = !!error;
+    const hasHelper = !!helperText;
+    return (
+        <FormControl
+            className={className}
+            disabled={disabled}
+            error={isError}
+            required={required}
+            id={id}
+            color={secondary ? 'secondary' : 'primary'}
+            fullWidth
+            margin="normal"
+            variant="filled"
+            >
+            <InputLabel id={`${id}-label`}>{label}</InputLabel>
+            <MuiSelect
+                value={value}
+                onChange={handleOnChangeEvent(value, onValueChange)}>
+                {(notSelectedText && value === '') &&
+                    <MenuItem value=''>
+                        {notSelectedText}
+                    </MenuItem>}
+                {options.map(value => (
+                    <MenuItem value={value} key={value}>
+                        {optionText[value]}
+                    </MenuItem>
+                ))}           
+            </MuiSelect>
+            {(isError || hasHelper) &&  
+            <FormHelperText>{error || helperText}</FormHelperText>}
+        </FormControl>
+    );
+}
+
+function create<Value extends string | number>() {
+    const compare = comparator<SelectProps<Value>>(changeableProps<Value>());
+    const S: React.FC<SelectProps<Value>> = Select;
+    const SelectMemo = React.memo(S, compare);
+    return SelectMemo;
+}
+
+export default create;
--- src/page/signIn/Page.tsx
+++ src/page/signIn/Page.tsx
@@ -4,6 +4,14 @@ import SingleForm from './../../component/layout/SingleForm';
 import LockIcon from '@material-ui/icons/LockOutlined';
 import Button from './../../component/Button';
 import Grid from '@material-ui/core/Grid';
+import TextArea from './../../component/formControl/TextArea';
+import NumberInput from './../../component/formControl/NumberInput';
+import CheckboxWithLabel from './../../component/formControl/CheckboxWithLabel';
+import createSelect from './../../component/formControl/Select';
+
+enum Test { A = 'A', B = 'B', C = 'C' };
+
+const Select = createSelect<Test>();
 
 const Page: React.FC<{}> = () => {
     return (
@@ -25,6 +33,38 @@ const Page: React.FC<{}> = () => {
                         text="Create organization" />
                 </Grid>
             </Grid>
+            <TextArea
+                initialRows={4}
+                maxSymbolsPerRow={75}
+                label="Test"
+                value="Malko tekst"
+                onValueChange={val => console.log(val)}
+                 />
+
+            <NumberInput
+                value={9}
+                label="Number"
+                onValueChange={val => console.log(val)} />
+
+            <CheckboxWithLabel
+                checked
+                label="Checkbox"
+                onToggle={toggle => console.log(toggle)} />
+
+            <Select
+                id="some-id"
+                value={''}
+                onValueChange={v => console.log(v)}
+                options={[ Test.A, Test.B, Test.C ]}
+                label="Select"
+                notSelectedText="Not selected"
+                optionText={{
+                    [Test.A]: "A t",
+                    [Test.B]: "B t",
+                    [Test.C]: "C t"
+                }}
+                error="Required"
+            />
         </SingleForm>
     );
 }
